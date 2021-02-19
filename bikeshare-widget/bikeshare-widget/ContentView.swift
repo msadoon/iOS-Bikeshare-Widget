1534d7ac6c496dfe8591553e7915163316e7ce8f
Mubarak Sadoon <msadoon@gmail.com>
2021-02-19 18:23:40 -0500

    added swiftui project template

---
+++ bikeshare-widget/bikeshare-widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bikeshare-widget
+//
+//  Created by Mubarak Sadoon on 2021-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bikeshare-widget/bikeshare-widget/bikeshare_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  bikeshare_widgetApp.swift
+//  bikeshare-widget
+//
+//  Created by Mubarak Sadoon on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct bikeshare_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
